06a04e6c1b721c55dcc924785f81e9dd2c526941
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-10 15:08:40 -0700

    Datos c++
    Primer enfoque de datos en c++ , datos básicos int, double, etc.

---
+++ Estructuras-Datos/datos.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+     int numero=15;
+     float mayor=3.2;
+     double menor=1.2;
+     char letra='a';
+
+     cout<<letra;
+     return 0;
+}
