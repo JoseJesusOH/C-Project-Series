16534c82d42915a03f10a7bf7847affd888e2b88
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-10 15:47:34 -0700

    C++ ejercicio edades
    El siguiente ejercicio de c++ es para practicar string o cadenas de texto char.

---
+++ Estructuras-Datos/edad.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int edad=0;
+    char sexo[20];
+    float altura;
+
+    cout<<"Ingresa tu edad: "; cin>>edad;
+    cout<<"Ingresa tu sexo: "; cin>>sexo;
+    cout<<"Ingresa tu altura: "; cin>>altura;
+
+    cout<<"Edad: "<<edad<<endl;
+    cout<<"sexo: "<<sexo<<endl;
+    cout<<"Altura: "<<altura<<endl;
+
+
+    return 0;
+}
