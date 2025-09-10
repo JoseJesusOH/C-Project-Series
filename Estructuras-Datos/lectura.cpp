797c6d1a38f88b0f5ba6040635eaf7824d7e3c3f
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-10 15:05:21 -0700

    Lectura de dos C++
    Código para la lectura de datos.

---
+++ Estructuras-Datos/lectura.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+/*
+Comentarios multiples lineas
+*/
+int main(){
+    int numero; //Comentario de una linea
+    cout<<"Digite un numero: ";
+    cin>>numero;
+    cout<<"Numero que digito es "<<numero;
+    return 0;
+}
